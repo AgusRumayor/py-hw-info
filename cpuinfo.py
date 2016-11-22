ab485e0972f02b82cc0b8425f6e9568b0aa8c34c
agus rumayor <agus@rumayor.org>
2016-11-22 21:32:55 +0100

    Double incr error

--- cpuinfo.py
+++ cpuinfo.py
@@ -1,10 +1,11 @@
 import sys, getopt
 import redis
 
-r = redis.StrictRedis(host='188.213.173.100', port=6379, db=0)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 pipe = r.pipeline()
 id = pipe.incr('id').get('id').execute()[1]
 npr =0
+r.set(str(id)+'_'+'num_processors', 0)
 for line in sys.stdin:
         lst = line.split(":")
         if len(lst)==2:
@@ -12,6 +13,5 @@ for line in sys.stdin:
                 v=lst[1]
                 v=v.rstrip()
                 if k == "processor":
-                        r.incr(str(id)+'_'+'num_processors')
                         npr = pipe.incr(str(id)+'_'+'num_processors').get(str(id)+'_'+'num_processors').execute()[1]
                 r.hset(str(id)+'_'+'p_'+str(npr),k,v)
