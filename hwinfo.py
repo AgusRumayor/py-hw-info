5b17839a755cd6c8a489eece8a86c3dd7bdef8cc
agus rumayor <agus@rumayor.org>
2016-11-23 17:47:53 +0100

    from /proc files

--- hwinfo.py
+++ hwinfo.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import sys
 import redis
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -6,7 +6,9 @@ pipe = r.pipeline()
 id = pipe.incr('id').get('id').execute()[1]
 npr =0
 r.set(str(id)+'_'+'num_processors', 0)
-for line in sys.stdin:
+cpufile = open('/proc/cpuinfo','r')
+for line in cpufile:
+	line = line.replace("\t", "")
         lst = line.split(":")
         if len(lst)==2:
                 k=lst[0]
